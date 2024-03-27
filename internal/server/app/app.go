e4c1c710ca8d63a250b3f5c7e17acdbd65df1489
Артем Садовщиков <artforteam@Artems-MacBook-Pro.local>
2024-03-27 19:55:33 +0300

    fix address

--- internal/server/app/app.go
+++ internal/server/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 
 	"flag"
 
@@ -12,16 +13,18 @@ import (
 
 func Run() {
 
-	addr := flag.String("a", "localhost:8080", "server address to listen on")
+	addrArg := flag.String("a", "localhost:8080", "server address to listen on")
 
+	re := regexp.MustCompile(`(localhost)|(127.0.0.1)`)
+	address := re.ReplaceAllString(*addrArg, "0.0.0.0")
 	flag.Parse()
 
 	r := chi.NewRouter()
 
 	routes.Init(r)
 
-	fmt.Println("server is listening on port 8080")
-	err := http.ListenAndServe(*addr, r)
+	fmt.Println("server is listening on:", address)
+	err := http.ListenAndServe(address, r)
 
 	if err != nil {
 		panic(err)
