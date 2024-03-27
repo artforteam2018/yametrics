5ebca9bc2e1d910af21576142bf61a3a4557c726
Артем Садовщиков <artforteam@Artems-MacBook-Pro.local>
2024-03-27 21:14:25 +0300

    fix

--- internal/agent/app/app.go
+++ internal/agent/app/app.go
@@ -1,3 +1,60 @@
+// package app
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"net"
+// 	"net/http"
+// 	"time"
+
+// 	"github.com/artforteam2018/yametrics/internal/agent/components/memstats"
+// 	"github.com/go-chi/chi"
+// )
+
+// func Run() {
+// 	memstats.Init()
+// 	r := chi.NewRouter()
+
+// 	go func() {
+// 		time.Sleep(1 * time.Second)
+// 		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+// 		defer cancel()
+// 		lc := &net.ListenConfig{}
+// 		fmt.Println("start listen")
+// 		ln, _ := lc.Listen(ctx, "tcp", ":8080")
+// 		fmt.Println(ln)
+// 		if ln != nil {
+// 			defer ln.Close()
+// 			done := make(chan struct{})
+// 			go func() {
+// 				fmt.Println("go esende")
+// 				conn, _ := ln.Accept()
+// 				fmt.Println("go esende2", conn)
+
+// 				if conn != nil {
+// 					_ = conn.Close()
+// 				}
+// 				close(done)
+// 			}()
+// 			select {
+// 			case <-done:
+// 				fmt.Println("done listen")
+// 				return
+// 			case <-ctx.Done():
+// 				fmt.Println("no listen")
+// 				fmt.Println(ctx.Err())
+// 			}
+// 		}
+// 	}()
+// 	fmt.Println("run")
+// 	err := http.ListenAndServe(`localhost:8080`, r)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// }
+
 package app
 
 import (
