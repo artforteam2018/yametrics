117dd8e9bf8fe1de4a74ba7d74a08e62a6c7db84
Артем Садовщиков <artforteam@Artems-MacBook-Pro.local>
2024-03-27 21:14:57 +0300

    fix

--- internal/agent/app/app.go
+++ internal/agent/app/app.go
@@ -59,30 +59,27 @@ package app
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
-	"regexp"
 
 	"github.com/artforteam2018/yametrics/internal/agent/components/memstats"
 )
 
 func Run() {
-	addrArg := flag.String("a", "localhost:8080", "server address to listen on")
+	// addrArg := flag.String("a", "localhost:8080", "server address to listen on")
 
 	reportInterval := flag.Int("r", 10, "report interval")
 	pollInterval := flag.Int("p", 2, "poll interval")
 
 	flag.Parse()
 
-	re := regexp.MustCompile(`(localhost)|(127.0.0.1)`)
-	address := re.ReplaceAllString(*addrArg, "")
+	// re := regexp.MustCompile(`(localhost)|(127.0.0.1)`)
+	// address := re.ReplaceAllString(*addrArg, "")
 
 	memstats.Init(*pollInterval, *reportInterval)
 
-	fmt.Println("server is listening on:", address)
-	err := http.ListenAndServe(address, nil)
+	// fmt.Println("server is listening on:", address)
+	// err := http.ListenAndServe(address, nil)
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
