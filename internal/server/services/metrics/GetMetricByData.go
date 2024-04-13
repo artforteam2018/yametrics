5f355b4d8817b6ff7ddc527293b0cb0f35577841
Артем Садовщиков <artforteam@Artems-MacBook-Pro.local>
2024-04-13 19:48:53 +0300

    fix

--- internal/server/services/metrics/GetMetricByData.go
+++ internal/server/services/metrics/GetMetricByData.go
@@ -14,10 +14,10 @@ type metricsGetRequest struct {
 }
 
 type metricsResponse struct {
-	ID    string  `json:"id"`              // имя метрики
-	MType string  `json:"type"`            // параметр, принимающий значение gauge или counter
-	Delta int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
-	Value float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+	ID    string  `json:"id"`    // имя метрики
+	MType string  `json:"type"`  // параметр, принимающий значение gauge или counter
+	Delta int64   `json:"delta"` // значение метрики в случае передачи counter
+	Value float64 `json:"value"` // значение метрики в случае передачи gauge
 }
 
 func GetMetricByData(w http.ResponseWriter, r *http.Request) {
